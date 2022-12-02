0fcedf4345757f5f74e1adee896dbc6d3dab4b1a
Karl Matthias <karl@matthias.org>
2022-12-02 16:39:57 +0000

    Begins tailing the logs

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/nxadm/tail v1.4.8
 	github.com/relistan/go-director v0.0.0-20200406104025-dbbf5d95248d
 	github.com/relistan/rubberneck v1.3.0
 	github.com/sirupsen/logrus v1.9.0
@@ -11,8 +12,10 @@ require (
 )
 
 require (
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 )
--- main.go
+++ main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-	//"github.com/nxadm/tail"
-
+	"fmt"
 	"time"
 
 	"github.com/kelseyhightower/envconfig"
+	"github.com/nxadm/tail"
 	director "github.com/relistan/go-director"
 	"github.com/relistan/rubberneck"
 	log "github.com/sirupsen/logrus"
 )
 
 type Config struct {
-	Environment string `envconfig:"ENVIRONMENT" default:"dev"`
-	BasePath    string `envconfig:"BASE_PATH" default:"/var/log/pods"`
+	Environment   string        `envconfig:"ENVIRONMENT" default:"dev"`
+	BasePath      string        `envconfig:"BASE_PATH" default:"/var/log/pods"`
+	DiscoInterval time.Duration `envconfig:"DISCO_INTERVAL" default:"3s"`
 }
 
 type PodTracker struct {
-	Pods map[string]*Pod
+	Pods     map[string]*Pod
+	LogTails map[string][]*tail.Tail
 
 	disco  Discoverer
 	looper director.Looper
@@ -25,9 +27,10 @@ type PodTracker struct {
 
 func NewPodTracker(looper director.Looper, disco Discoverer) *PodTracker {
 	return &PodTracker{
-		Pods:   make(map[string]*Pod, 5),
-		looper: looper,
-		disco:  disco,
+		Pods:     make(map[string]*Pod, 5),
+		LogTails: make(map[string][]*tail.Tail, 5),
+		looper:   looper,
+		disco:    disco,
 	}
 }
 
@@ -42,10 +45,22 @@ func (t *PodTracker) Run() {
 		newPods := make(map[string]*Pod, len(t.Pods))
 
 		for _, pod := range discovered {
+			// Handle newly discovered pods
 			if _, ok := t.Pods[pod.Name]; !ok {
 				log.Infof("new pod --> %s:%s  [%s]", pod.Namespace, pod.ServiceName, pod.Name)
 
+				logFiles, err := t.disco.LogFiles(pod.Name)
+				if err != nil {
+					log.Warnf("Failed to get logs for pod %s: %s", pod.Name, err)
+					continue
+				}
+
 				newPods[pod.Name] = pod
+				err = t.tailLogs(pod.Name, logFiles)
+				if err != nil {
+					log.Warnf("Failed to tail logs for pod %s: %s", pod.Name, err)
+					continue
+				}
 				continue
 			}
 
@@ -68,6 +83,20 @@ func (t *PodTracker) Run() {
 	})
 }
 
+func (t *PodTracker) tailLogs(podName string, logFiles []string) error {
+	for _, filename := range logFiles {
+		tailed, err := tail.TailFile(filename, tail.Config{ReOpen: true, Follow: true})
+		if err != nil {
+			return fmt.Errorf("failed to tail log for %s: %w", podName, err)
+		}
+
+		log.Infof("  Adding tail on %s for pod %s", filename, podName)
+		t.LogTails[podName] = append(t.LogTails[podName], tailed)
+	}
+
+	return nil
+}
+
 func main() {
 	var config Config
 	err := envconfig.Process("log", &config)
@@ -77,8 +106,10 @@ func main() {
 	rubberneck.Print(config)
 
 	disco := NewDirListDiscoverer(config.BasePath, config.Environment)
-	podDiscoveryLooper := director.NewImmediateTimedLooper(director.FOREVER, 3*time.Second, make(chan error))
+	podDiscoveryLooper := director.NewImmediateTimedLooper(director.FOREVER, config.DiscoInterval, make(chan error))
+
 	tracker := NewPodTracker(podDiscoveryLooper, disco)
 	go tracker.Run()
+
 	podDiscoveryLooper.Wait()
 }
